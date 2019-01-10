8968a259cb1baae28b9ea178132bd79e528e93f3
ColdFray <a13883490832@126.com>
2019-01-10 09:18:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/qw/memo/OneMemo.java
@@ -0,0 +1,50 @@
+package com.example.qw.memo;
+
+
+public class OneMemo {
+    private int tag;
+    private String textDate;
+    private String textTime;
+    private boolean alarm;
+    private String mainText;
+
+    public OneMemo(int tag, String textDate, String textTime,boolean alarm, String mainText) {
+        this.tag=tag;
+        this.textDate=textDate;
+        this.textTime=textTime;
+        this.alarm=alarm;
+        this.mainText=mainText;
+    }
+
+    //getter
+    public int getTag(){
+        return tag;
+    }
+    public String getTextDate(){
+        return textDate;
+    }
+    public String getTextTime(){
+        return textTime;
+    }
+    public boolean getAlarm(){ return alarm; }
+    public String getMainText(){
+        return mainText;
+    }
+
+    //setter
+    public void setTag(int tag){
+        this.tag=tag;
+    }
+    public void setTextDate(String textDate){
+        this.textDate=textDate;
+    }
+    public void setTextTime(String textTime){
+        this.textTime=textTime;
+    }
+    public void setAlarm(boolean alarm){
+        this.alarm=alarm;
+    }
+    public void setMainText(String mainText){
+        this.mainText=mainText;
+    }
+}
